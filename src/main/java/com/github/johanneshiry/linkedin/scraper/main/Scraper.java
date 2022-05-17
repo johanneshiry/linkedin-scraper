5f74d20c1c1a85286597dbca27e795227faa1a19
Johannes Hiry <johannes.hiry@tu-dortmund.de>
2022-05-17 18:25:09 +0200

    first Scraper implementation

---
+++ src/main/java/com/github/johanneshiry/linkedin/scraper/main/Scraper.java
@@ -0,0 +1,172 @@
+/*
+ * © 2022. Johannes Hiry
+ */
+
+package com.github.johanneshiry.linkedin.scraper.main;
+
+import static com.github.johanneshiry.linkedin.scraper.model.LinkedInConstants.*;
+
+import com.github.johanneshiry.linkedin.scraper.model.Contact;
+import com.github.johanneshiry.linkedin.scraper.model.SimpleContact;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.support.ui.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Scraper {
+
+  private static final Logger log = LoggerFactory.getLogger(Scraper.class);
+
+  private static final Duration BROWSER_WAIT_TIMEOUT = Duration.of(10, ChronoUnit.SECONDS);
+
+  public static List<SimpleContact> getSimpleConnections(
+      List<String> names, String loginUsername, String loginPassword, WebDriver driver) {
+    // used to reset provided driver afterwards
+    String initialUrl = driver.getCurrentUrl();
+
+    try {
+      loginToLinkedIn(driver, loginUsername, loginPassword);
+    } catch (Exception e) {
+      log.error("Cannot login to LinkedIn. ", e);
+      return Collections.emptyList();
+    }
+
+    List<SimpleContact> connections = getConnectionsByNames(names, driver);
+
+    // reset webdriver
+    driver.get(initialUrl);
+
+    return connections;
+  }
+
+  public static Optional<Contact> getSimpleConnection(
+      String name, String loginUsername, String loginPassword, WebDriver driver) {
+    // used to reset provided driver afterwards
+    String initialUrl = driver.getCurrentUrl();
+
+    try {
+      loginToLinkedIn(driver, loginUsername, loginPassword);
+    } catch (Exception e) {
+      log.error("Cannot login to LinkedIn. ", e);
+      return Optional.empty();
+    }
+
+    Optional<Contact> maybeContact = getConnectionByName(name, driver);
+
+    // reset webdriver
+    driver.get(initialUrl);
+
+    return maybeContact;
+  }
+
+  public static int getNoOfContacts(String loginUsername, String loginPassword, WebDriver driver) {
+    // used to reset provided driver afterwards
+    String initialUrl = driver.getCurrentUrl();
+
+    try {
+      loginToLinkedIn(driver, loginUsername, loginPassword);
+    } catch (Exception e) {
+      log.error("Cannot login to LinkedIn. ", e);
+      return -1;
+    }
+
+    openConnectionsPage(driver);
+
+    WebElement header = driver.findElement(LINKED_IN_CONNECTIONS_HEADER_CLASS_NAME);
+    String headerString = header.findElement(By.xpath("//header/h1")).getAccessibleName();
+
+    // reset webdriver
+    driver.get(initialUrl);
+
+    return Integer.parseInt(headerString.replaceAll("[\\D]", ""));
+  }
+
+  private static void loginToLinkedIn(
+      WebDriver driver, String loginUsername, String loginPassword) {
+
+    driver.get(LOGIN_URL);
+    new WebDriverWait(driver, BROWSER_WAIT_TIMEOUT)
+        .until(ExpectedConditions.presenceOfElementLocated(LOGIN_USERNAME));
+
+    WebElement eMail = driver.findElement(LOGIN_USERNAME);
+    eMail.sendKeys(loginUsername);
+    WebElement password = driver.findElement(LOGIN_PASSWORD);
+    password.sendKeys(loginPassword);
+
+    password.submit();
+  }
+
+  private static void openConnectionsPage(WebDriver driver) {
+    driver.get(CONNECTIONS_URL);
+
+    new WebDriverWait(driver, BROWSER_WAIT_TIMEOUT)
+        .until(ExpectedConditions.presenceOfElementLocated(CONNECTIONS_SECTION_CLASS_NAME));
+  }
+
+  private static Optional<Contact> getConnectionByName(String name, WebDriver driver) {
+    try {
+      List<SimpleContact> contacts = getConnectionsByNames(List.of(name), driver);
+      if (contacts.size() > 1) {
+        log.warn(
+            "Multiple LinkedIn connections for name '{}' found. Returning the first one found!",
+            name);
+      }
+      return Optional.ofNullable(contacts.get(0));
+    } catch (IndexOutOfBoundsException e) {
+      return Optional.empty();
+    }
+  }
+
+  private static List<SimpleContact> getConnectionsByNames(List<String> names, WebDriver driver) {
+    openConnectionsPage(driver);
+
+    final WebElement search = driver.findElement(CONNECTIONS_SEARCH_INPUT);
+
+    final Wait<WebDriver> wait =
+        new FluentWait<>(driver)
+            .withTimeout(Duration.ofSeconds(3L))
+            .pollingEvery(Duration.ofMillis(500L))
+            .ignoring(NoSuchElementException.class)
+            .ignoring(StaleElementReferenceException.class);
+
+    return names.stream()
+        .flatMap(
+            name -> {
+
+              // search for name
+              search.sendKeys(name);
+
+              List<SimpleContact> linkedInContacts = Collections.emptyList();
+              try {
+                linkedInContacts =
+                    wait.until(
+                        webDriver -> {
+                          List<SimpleContact> foundContacts =
+                              webDriver.findElements(CONNECTIONS_CARD_CLASS_NAME).stream()
+                                  .map(SimpleContact::from)
+                                  .flatMap(Optional::stream)
+                                  .filter(simpleContact -> simpleContact.name().equals(name))
+                                  .toList();
+                          if (foundContacts.isEmpty()) {
+                            return null; // keep waiting
+                          }
+                          return foundContacts;
+                        });
+              } catch (TimeoutException e) {
+                log.info(
+                    "Cannot query information for contact '{}'. No LinkedIn connection found!",
+                    name);
+              }
+
+              // clear search input
+              search.clear();
+
+              return linkedInContacts.stream();
+            })
+        .toList();
+  }
+}
---
+++ src/main/java/com/github/johanneshiry/linkedin/scraper/model/LinkedInConstants.java
@@ -0,0 +1,39 @@
+/*
+ * © 2022. Johannes Hiry
+ */
+
+package com.github.johanneshiry.linkedin.scraper.model;
+
+import org.openqa.selenium.By;
+
+public class LinkedInConstants {
+
+  /** login window */
+  public static final String LOGIN_URL = "https://www.linkedin.com/login";
+
+  public static final By LOGIN_USERNAME = By.id("username");
+
+  public static final By LOGIN_PASSWORD = By.id("password");
+
+  /** network / connections window */
+  public static final String CONNECTIONS_URL =
+      "https://www.linkedin.com/mynetwork/invite-connect/connections/";
+
+  public static final By CONNECTIONS_SECTION_CLASS_NAME = By.className("mn-connections");
+
+  public static final By LINKED_IN_CONNECTIONS_HEADER_CLASS_NAME =
+      By.className("mn-connections__header");
+
+  public static final By CONNECTIONS_SEARCH_INPUT = By.className("mn-connections__search-input");
+
+  public static final By CONNECTIONS_CARD_CLASS_NAME = By.className("mn-connection-card");
+
+  public static final By CONNECTION_CARD_DETAILS = By.className("mn-connection-card__details");
+
+  public static final By CONNECTION_CARD_NAME = By.className("mn-connection-card__name");
+
+  public static final By CONNECTION_CARD_URL = By.className("mn-connection-card__link");
+
+  public static final By CONNECTION_CARD_OCCUPATION =
+      By.className("mn-connection-card__occupation");
+}
